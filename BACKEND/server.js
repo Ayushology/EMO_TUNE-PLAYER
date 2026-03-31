9ca0b42b4ab31a93b0ee0c7dc5a3d54a6cada7f3
Ayush Kumar <ash09aps@gmail.com>
2026-03-31 20:15:12 +0530

    fixed port for deployment

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 
-const app = require('./src/app')
-const connecttodb = require('./src/db/db')
-connecttodb();
-app.listen(3000,(req,res)=>{
-    console.log("SERVER IS LISTENING AT PORT 3000");  
-})
+const app = require('./src/app');
+const connecttodb = require('./src/db/db');
 
+connecttodb();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`SERVER IS LISTENING AT PORT ${PORT}`);
+});
